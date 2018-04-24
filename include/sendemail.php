74b7e9b8344c2088d01612df8677261b5b4ad451
rohit715131 <33005806+rohit715131@users.noreply.github.com>
2018-04-24 23:33:19 +0530

    first website upload

---
+++ include/sendemail.php
@@ -0,0 +1,102 @@
+<?php
+
+require_once('phpmailer/PHPMailerAutoload.php');
+
+$toemails = array();
+
+$toemails[] = array(
+				'email' => 'Anees.parts2repair@gmail.com', // Your Email Address
+				
+				'name' => 'Aneesh' // Your Name
+			);
+
+// Form Processing Messages
+$message_success = 'We have <strong>successfully</strong> received your Message and will get Back to you as soon as possible.';
+
+// Add this only if you use reCaptcha with your Contact Forms
+$recaptcha_secret = 'your-recaptcha-secret-key'; // Your reCaptcha Secret
+
+$mail = new PHPMailer();
+
+// If you intend you use SMTP, add your SMTP Code after this Line
+
+
+if( $_SERVER['REQUEST_METHOD'] == 'POST' ) {
+	if( $_POST['template-contactform-email'] != '' ) {
+
+		$name = isset( $_POST['template-contactform-name'] ) ? $_POST['template-contactform-name'] : '';
+		$email = isset( $_POST['template-contactform-email'] ) ? $_POST['template-contactform-email'] : '';
+		$carModel = isset( $_POST['template-contactform-model'] ) ? $_POST['template-contactform-model'] : '';
+		$year = isset( $_POST['template-contactform-year'] ) ? $_POST['template-contactform-year'] : '';
+		$phone = isset( $_POST['template-contactform-phone'] ) ? $_POST['template-contactform-phone'] : '';
+		$brand = isset( $_POST['template-contactform-brand'] ) ? $_POST['template-contactform-brand'] : '';
+		$varient = isset( $_POST['template-contactform-varient'] ) ? $_POST['template-contactform-varient'] : '';
+		$subject = 'from carbeccho ';
+		// $message = isset( $_POST['template-contactform-message'] ) ? $_POST['template-contactform-message'] : '';
+
+
+
+		$subject = isset($subject) ? $subject : 'New Message From Contact Form';
+
+		$botcheck = $_POST['template-contactform-botcheck'];
+
+		if( $botcheck == '' ) {
+
+			$mail->SetFrom( $email , $name );
+			$mail->AddReplyTo( $email , $name );
+			foreach( $toemails as $toemail ) {
+				$mail->AddAddress( $toemail['email'] , $toemail['name'] );
+			}
+			$mail->Subject = $subject;
+
+			$name = isset($name) ? "Name: $name<br><br>" : '';
+			$email = isset($email) ? "Email: $email<br><br>" : '';
+			$carModel = isset($carModel) ? "Car Model: $carModel<br><br>" : '';
+			$year = isset($year) ? "Year: $year<br><br>" : '';
+			$phone = isset($phone) ? "Phone: $phone<br><br>" : '';
+			$brand = isset($brand) ? "Brand: $brand<br><br>" : '';
+			$varient = isset($varient) ? "Brand: $brand<br><br>" : '';
+			// $message = isset($message) ? "Message: $message<br><br>" : '';
+
+			$referrer = $_SERVER['HTTP_REFERER'] ? '<br><br><br>This Form was submitted from: ' . $_SERVER['HTTP_REFERER'] : '';
+
+			$body = "$name $email $carModel $year $phone $brand  $referrer $varient";
+
+			// Runs only when File Field is present in the Contact Form
+			if ( isset( $_FILES['template-contactform-file'] ) && $_FILES['template-contactform-file']['error'] == UPLOAD_ERR_OK ) {
+				$mail->IsHTML(true);
+				$mail->AddAttachment( $_FILES['template-contactform-file']['tmp_name'], $_FILES['template-contactform-file']['name'] );
+			}
+
+			// Runs only when reCaptcha is present in the Contact Form
+			if( isset( $_POST['g-recaptcha-response'] ) ) {
+				$recaptcha_response = $_POST['g-recaptcha-response'];
+				$response = file_get_contents( "https://www.google.com/recaptcha/api/siteverify?secret=" . $recaptcha_secret . "&response=" . $recaptcha_response );
+
+				$g_response = json_decode( $response );
+
+				if ( $g_response->success !== true ) {
+					echo '{ "alert": "error", "message": "Captcha not Validated! Please Try Again." }';
+					die;
+				}
+			}
+
+			$mail->MsgHTML( $body );
+			$sendEmail = $mail->Send();
+
+			if( $sendEmail == true ):
+				echo '{ "alert": "success", "message": "' . $message_success . '" }';
+			else:
+				echo '{ "alert": "error", "message": "Email <strong>could not</strong> be sent due to some Unexpected Error. Please Try Again later.<br /><br /><strong>Reason:</strong><br />' . $mail->ErrorInfo . '" }';
+			endif;
+		} else {
+			echo '{ "alert": "error", "message": "Bot <strong>Detected</strong>.! Clean yourself Botster.!" }';
+		}
+	} else {
+		echo '{ "alert": "error", "message": "Please <strong>Fill up</strong> all the Fields and Try Again." }';
+	}
+} else {
+	echo '{ "alert": "error", "message": "An <strong>unexpected error</strong> occured. Please Try Again later." }';
+}
+
+?>
